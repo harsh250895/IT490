ebc963077110c37b9dd82301ccf6daa8f43aacc8
root <harsh250895@gmail.com>
2018-03-07 18:29:16 -0500

    added location variable

--- speciality.php
+++ speciality.php
@@ -4,8 +4,9 @@ ini_set('display_errors', true);
 
 include ('client.php');
 $speciality = $_GET['speciality'];
+$location = $_GET['location'];
 //$uid = '333d4bb6fcf640e18e93b11b00fe09eb';
-$response = searchSpeciality($speciality);
+$response = searchSpeciality($speciality,$location);
 
 $respo = rtrim($response);
 header('Content-Type: application/json;charset=utf-8');
