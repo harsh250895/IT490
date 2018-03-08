5a3589c14a936c9d28bb55a1f436d604552298cb
harsh250895 <harsh250895@gmail.com>
2018-03-08 12:08:47 -0500

    changed links

--- specialtySearch.php
+++ specialtySearch.php
@@ -109,13 +109,13 @@ form{
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">Primary Doctor</a>
+            <a class="nav-link js-scroll-trigger" href="primary.php">Primary Doctor</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#services">Visited</a>
+            <a class="nav-link js-scroll-trigger" href="visited.php">Visited</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#portfolio">To-be Visited</a>
+            <a class="nav-link js-scroll-trigger" href="tobevisited.php">To-be Visited</a>
           </li>
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="logout.php">Logout</a>
