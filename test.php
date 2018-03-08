5abb9d0694f90a3f09a787c805568ad4a97ad49f
root <harsh250895@gmail.com>
2018-03-08 00:41:28 -0500

    added css links

--- test.php
+++ test.php
@@ -9,18 +9,30 @@ if (!isset($_SESSION["user"])){
  header( "Refresh:1; url=login.html", true, 303);
  }
 
-$uid = $_GET['lic'];?>
+?>
 <!DOCTYPE html>
 <html>
 
 
 <head>
   <meta charset="utf-8">
+  <link href="startbootstrap-creative-gh-pages/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="startbootstrap-creative-gh-pages/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+  <!-- Plugin CSS -->
+  <link href="startbootstrap-creative-gh-pages/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="startbootstrap-creative-gh-pages/css/creative.min.css" rel="stylesheet">
   <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js"></script>
   <script type="text/javascript" >
 
-  var uid ="<?php echo $uid;?>";
+  var uid ="<?php $uid = $_GET['lic']; echo $uid;?>";
   var resource_url = 'search.php?uid='+ uid;
   $.get(resource_url, function(data) {
 
