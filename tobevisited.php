52142e547d0dbc408c9e931ad81bf79ce9a57257
root <harsh250895@gmail.com>
2018-03-08 04:11:40 -0500

    testing

--- tobevisited.php
+++ tobevisited.php
@@ -1,7 +1,13 @@
-<?
+<?php
 
-$user = "user";
-$response = getlist($user);
-echo $response;
+$name = $_GET['name'];
+$user = $_GET['user'];
+$uid = $_GET['uid'];
+$date = $_GET['date'];
+echo $user;
+echo $uid;
+echo $name;
+echo $date;
+
 
 ?>
