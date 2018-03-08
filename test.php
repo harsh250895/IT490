9f61ff2932c43851c3b6d5e82032eadb89d2abdf
root <harsh250895@gmail.com>
2018-03-08 00:18:03 -0500

    session and log

--- test.php
+++ test.php
@@ -1,4 +1,14 @@
-<?php $uid = $_GET['lic'];?>
+<?php
+
+session_start();
+ini_set("display_errors", 1);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+if (!isset($_SESSION["user"])){
+ header( "Refresh:1; url=login.html", true, 303);
+ }
+$uid = $_GET['lic'];?>
 <!DOCTYPE html>
 <html>
 
