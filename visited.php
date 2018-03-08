b80c4d92ac29c9e1be6099c826602ab9117066a4
root <harsh250895@gmail.com>
2018-03-08 09:19:21 -0500

    added visited list

---
+++ visited.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+ini_set("display_errors", 1);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+include ('client.php');
+if (!isset($_SESSION["user"])){
+ header( "Refresh:1; url=login.html", true, 303);
+ }
+$type = $_GET['type'];
+ if($type == "rm"){
+
+$uid = $_GET['uid'];
+
+$response = rmAP($uid,$_SESSION['user']);
+echo $response;
+}
+else{
+$response = newList($_SESSION['user']);
+echo $response;
+}
+?>
