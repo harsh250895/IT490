2b6e2704022393b4e8a6667601aadd18711959d1
harsh250895 <harsh250895@gmail.com>
2018-03-10 01:25:11 -0500

    for adding or viewing primary

---
+++ primarydoc.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+ini_set("display_errors", 1);
+ini_set("log_errors",1);
+ini_set("error_log", "/tmp/error.log");
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_STRICT);
+if (!isset($_SESSION["user"])){
+ header( "Refresh:1; url=login.html", true, 303);
+ }
+
+include ('client.php');
+$type=$_GET['type'];
+
+if($type == "add"){
+$uid = $_GET['uid'];
+//$uid = '333d4bb6fcf640e18e93b11b00fe09eb';
+$response = addPri($uid,$_SESSION['user']);
+
+$respo = rtrim($response);
+header('Content-Type: application/json;charset=utf-8');
+
+//echo json_decode($data);
+echo ($respo);}
+else{
+  $response = getPri($_SESSION['user']);
+
+  $respo = rtrim($response);
+  header('Content-Type: application/json;charset=utf-8');
+
+  //echo json_decode($data);
+  echo ($respo);
+
+}
+?>
