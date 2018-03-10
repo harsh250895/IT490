fc4a2a4b3d042e3e614f6df02e1974afcc7c5c1f
harsh250895 <harsh250895@gmail.com>
2018-03-10 17:35:39 -0500

    added navigation bar

--- test.php
+++ test.php
@@ -124,17 +124,16 @@ form{
 </head>
 <body id="page-top">
 
-  <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">DocRx</a>
+      <a class="navbar-brand js-scroll-trigger" href="searchpage.php">DocRx</a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="primary.php">Primary Doctor</a>
+            <a class="nav-link js-scroll-trigger" href="primarydoc.php">Primary</a>
           </li>
           <li class="nav-item">
             <a class="nav-link js-scroll-trigger" href="visited.php">Visited</a>
