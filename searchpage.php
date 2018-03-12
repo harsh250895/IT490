f7a5e660609d314a7cc33018063ab32048f8f118
harsh250895 <harsh250895@gmail.com>
2018-03-12 15:14:53 -0400

    added search page from front end

--- searchpage.php
+++ searchpage.php
@@ -110,36 +110,36 @@ if (!isset($_SESSION["user"])){
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
               <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Sturdy Templates</h3>
-              <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p>
+              <h3 class="mb-3">Qualified Doctors</h3>
+              <p class="text-muted mb-0">Our services will provide you with a list of the most experienced doctors.</p>
             </div>
           </div>
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
               <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Ready to Ship</h3>
-              <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
+              <h3 class="mb-3">Make a appointment</h3>
+              <p class="text-muted mb-0">Make an appointment on the go</p>
             </div>
           </div>
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
               <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Up to Date</h3>
-              <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
+              <h3 class="mb-3">Notifications</h3>
+              <p class="text-muted mb-0">Get notifications of your upcoming visit</p>
             </div>
           </div>
           <div class="col-lg-3 col-md-6 text-center">
             <div class="service-box mt-5 mx-auto">
               <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
-              <h3 class="mb-3">Made with Love</h3>
-              <p class="text-muted mb-0">You have to make your websites with love these days!</p>
+              <h3 class="mb-3">Care</h3>
+              <p class="text-muted mb-0">Round the clock customer service</p>
             </div>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="p-0" id="portfolio">
+  <!--  <section class="p-0" id="portfolio">
       <div class="container-fluid p-0">
         <div class="row no-gutters popup-gallery">
           <div class="col-lg-4 col-sm-6">
@@ -235,13 +235,12 @@ if (!isset($_SESSION["user"])){
         </div>
       </div>
     </section>
-
     <section class="bg-dark text-white">
       <div class="container text-center">
         <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
         <a class="btn btn-light btn-xl sr-button" href="http://startbootstrap.com/template-overviews/creative/">Download Now!</a>
       </div>
-    </section>
+    </section> -->
 
     <section id="contact">
       <div class="container">
@@ -249,7 +248,7 @@ if (!isset($_SESSION["user"])){
           <div class="col-lg-8 mx-auto text-center">
             <h2 class="section-heading">Let's Get In Touch!</h2>
             <hr class="my-4">
-            <p class="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+            <p class="mb-5">Let us solve your problems. Your suggestions for iprovements are always welcomed!</p>
           </div>
         </div>
         <div class="row">
@@ -260,7 +259,7 @@ if (!isset($_SESSION["user"])){
           <div class="col-lg-4 mr-auto text-center">
             <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
             <p>
-              <a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a>
+              <a href="mailto:your-email@your-domain.com">docrx@email.com</a>
             </p>
           </div>
         </div>
